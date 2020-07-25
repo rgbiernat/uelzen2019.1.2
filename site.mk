b94efc9abd852919ffaeb307f3bdc3cbaabcb58e
rgbiernat <info@24prompt.de>
2020-07-25 16:56:27 +0200

    Update site.mk
    GLUON_DEPRECATED ?= upgrade

--- site.mk
+++ site.mk
@@ -42,7 +42,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := uelzen-1.07
+DEFAULT_GLUON_RELEASE := uelzen-1.08
 
 
 ##	GLUON_RELEASE
@@ -54,6 +54,7 @@ DEFAULT_GLUON_RELEASE := uelzen-1.07
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+GLUON_DEPRECATED ?= upgrade
 
 GLUON_PRIORITY ?= 0
 
