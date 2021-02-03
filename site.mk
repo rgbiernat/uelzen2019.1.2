85f45000e09e086bf4c5e2d39553da4aa44412da
rgbiernat <info@24prompt.de>
2021-02-03 21:53:38 +0100

    Geändert.

--- site.mk
+++ site.mk
@@ -42,7 +42,7 @@ GLUON_SITE_PACKAGES := \
 #			opkg compare-versions "$1" '>>' "$2"
 #		to decide if a version is newer or not.
 
-DEFAULT_GLUON_RELEASE := uelzen-1.08
+DEFAULT_GLUON_RELEASE := bienenbuettel-1.08
 
 
 ##	GLUON_RELEASE
